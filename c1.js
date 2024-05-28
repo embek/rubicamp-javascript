851925f090ab57a43878fb3669b48b1ae74b56e9
Adzka <adzka2008@gmail.com>
2024-05-28 15:09:07 +0700

    commit c1.js

---
+++ c1.js
@@ -0,0 +1,14 @@
+function sum() {
+    //write code here
+    //console.log(arguments)
+    var total = 0;
+    for (let i = 0; i < arguments.length; i++) {
+        total += arguments[i];
+    }
+    console.log(total)
+}
+
+sum(1, 2, 7);
+sum(1, 4);
+sum(11);
+sum(10, 3, 6, 7, 9);
