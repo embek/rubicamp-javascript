ff90466040ad93eae8d7535f2184088a45a09004
Adzka <adzka2008@gmail.com>
2024-05-28 15:43:55 +0700

    commit c9.js

---
+++ c9.js
@@ -0,0 +1,13 @@
+function spiral(param1) {
+    // Your code here
+    var x = param1, arr ;
+    for (let i = 0; i < param1; i++) {
+        for (let j = 0; j < param1; j++) {
+            arr[i][j] = param;
+        }
+    }
+}
+
+spiral(5);
+spiral(6);
+spiral(7);
